074bf6aa146667c0a9a459cfdea5f2a796046e07
wawanneutron <wawanneutron1331@gmail.com>
2026-05-13 14:15:23 +0700

    feat: add users sorting feature

---
+++ components/users/sort-select.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  value: string
+  onChange: (value: string) => void
+}
+
+export default function SortSelect({
+  value,
+  onChange,
+}: Props) {
+  return (
+    <select
+      value={value}
+      onChange={(e) =>
+        onChange(e.target.value)
+      }
+      className="rounded-lg border px-4 py-2"
+    >
+      <option value="asc">
+        Name A-Z
+      </option>
+
+      <option value="desc">
+        Name Z-A
+      </option>
+    </select>
+  )
+}

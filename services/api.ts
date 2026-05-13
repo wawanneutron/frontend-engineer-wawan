972ca58583d3885f94e4cf1520bf492aa324c1c7
wawanneutron <wawanneutron1331@gmail.com>
2026-05-13 12:00:11 +0700

    feat: setup project foundation

---
+++ services/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+import { User } from "@/types/user";
+
+const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+export async function fetchUsers(): Promise<User[]> {
+  const response = await api.get("/users");
+  return response.data;
+}
+
+export async function fetchUserById(id: string): Promise<User> {
+  const response = await api.get(`/users/${id}`);
+  return response.data;
+}
+
+export async function fetchPosts() {
+  const response = await api.get("/posts");
+  return response.data;
+}
+
+export async function fetchTodos() {
+  const response = await api.get("/todos");
+  return response.data;
+}
---
+++ types/user.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+
+  company: {
+    name: string;
+    catchPhrase: string;
+  };
+}

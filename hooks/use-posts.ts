2e4e69bd8ad5cb329c6e83171aa5d1334a8247fe
wawanneutron <wawanneutron1331@gmail.com>
2026-05-13 16:40:35 +0700

    feat: add posts queries hook

---
+++ hooks/use-posts.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "@/services/api";
+
+export function usePosts() {
+  return useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+}

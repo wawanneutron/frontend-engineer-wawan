be3ddf9c71315c007ad65dc52e01bbad03aae3b0
wawanneutron <wawanneutron1331@gmail.com>
2026-05-13 22:53:08 +0700

    fix: formater prettier

--- services/api.ts
+++ services/api.ts
@@ -1,27 +1,29 @@
-import axios from "axios";
+import axios from 'axios'
 
-import { User } from "@/types/user";
+import { User } from '@/types/user'
+import { Post } from '@/types/post'
+import { Todo } from '@/types/todo'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-});
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
+})
 
 export async function fetchUsers(): Promise<User[]> {
-  const response = await api.get("/users");
-  return response.data;
+  const response = await api.get('/users')
+  return response.data
 }
 
-export async function fetchUserById(id: string): Promise<User> {
-  const response = await api.get(`/users/${id}`);
-  return response.data;
+export async function fetchUserById(id: number): Promise<User> {
+  const response = await api.get(`/users/${id}`)
+  return response.data
 }
 
-export async function fetchPosts() {
-  const response = await api.get("/posts");
-  return response.data;
+export async function fetchPosts(): Promise<Post[]> {
+  const response = await api.get('/posts')
+  return response.data
 }
 
-export async function fetchTodos() {
-  const response = await api.get("/todos");
-  return response.data;
+export async function fetchTodos(): Promise<Todo[]> {
+  const response = await api.get('/todos')
+  return response.data
 }

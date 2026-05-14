1963164105cefece35948e04f33387724e2d40ac
wawanneutron <wawanneutron1331@gmail.com>
2026-05-14 15:44:29 +0700

    style: enhance style background global

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} font-sans bg-linear-to-br from-slate-50 to-slate-100 text-slate-900 antialiased min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} font-sans text-slate-900 antialiased min-h-screen`}
       >
         <QueryProvider>
           <main className="mx-auto max-w-7xl space-y-8 p-4 md:p-8">

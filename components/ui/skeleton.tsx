06ae3c91518dd278e1313769ff09bbeb35c9e827
wawanneutron <wawanneutron1331@gmail.com>
2026-05-14 09:28:37 +0700

    feat: add skeleton component ui reuseable

---
+++ components/ui/skeleton.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  className?: string
+}
+
+export default function Skeleton({ className = '' }: Props) {
+  return (
+    <div className={`animate-pulse rounded-md bg-gray-200 ${className}`} />
+  )
+}
